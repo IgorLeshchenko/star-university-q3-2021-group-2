8cc72ca33998c4884022ddccc6e96394da26367a
Yana Gushlevskaya <jasynja@ukr.net>
2021-11-05 14:16:41 +0200

    add button to test modal (should be removed in the future)

--- src/pages/posts/Posts.tsx
+++ src/pages/posts/Posts.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import { Button } from '../../components/Button'
+import { ErrorModal } from '../../components/ErrorModal'
 import { Header } from '../../components/Header'
 
 import styles from './Posts.module.scss'
 
 export const Posts = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const modalHandler = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <React.Fragment>
       <Header />
-      <div className={styles.Posts}>Forum page</div>
+      <div className={styles.Posts}>
+        Forum page <br />
+        <Button primary={true} onClick={modalHandler}>
+          Error modal
+        </Button>
+      </div>
+      {isOpen && <ErrorModal onCrossBtnHandler={modalHandler} />}
     </React.Fragment>
   )
 }
