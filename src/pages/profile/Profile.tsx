f131f5ae6b6924301bb2d70c042698b4b1b39233
KolomiietsAn <kkolomiiets@star.global>
2021-11-09 19:52:08 +0200

    feature/#42: Add loading and not found states

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -5,6 +5,7 @@ import { PublicRequestsService } from '../../API/PublicRequestsService'
 import { Avatar } from '../../components/Avatar'
 import { Container } from '../../components/Container'
 import { Header } from '../../components/Header'
+import { Spinner } from '../../components/Spinner'
 import { IUser } from '../../models/User'
 
 import styles from './Profile.module.scss'
@@ -19,11 +20,20 @@ interface IProfile {
 }
 export const Profile: React.FC<React.PropsWithChildren<IProfile>> = ({ match }) => {
   const [user, setUser] = useState<IUser | null>(null)
+  const [loading, setLoading] = useState(true)
+  const [isNotFound, setNotFound] = useState(false)
   const { username } = match.params
 
   const getUser = async (username: string) => {
-    const response = await PublicRequestsService.getUserPublicData({ username })
-    setUser(response.data)
+    try {
+      setLoading(true)
+      const response = await PublicRequestsService.getUserPublicData({ username })
+      setUser(response.data)
+      setLoading(true)
+    } catch (error) {
+      setLoading(false)
+      setNotFound(true)
+    }
   }
 
   useEffect(() => {
@@ -35,28 +45,33 @@ export const Profile: React.FC<React.PropsWithChildren<IProfile>> = ({ match }) 
   const isAuthorized = true
   const authorizedLogin = 'testertest'
 
-  const isPageOwner = (): boolean => {
-    return username === authorizedLogin
-  }
-
+  const isPageOwner = username === authorizedLogin
   return (
     <React.Fragment>
       <Header />
       <Container>
-        <div className={styles.profile}>
-          <p className={styles.profile_title}>Profile</p>
-          <div className={styles.profile_content}>
-            <div className={styles.profile_media}>
-              <Avatar imageUrl={resolveProfileImagePath(username)} customClass={styles.profile_avatar} />
-              {isAuthorized && isPageOwner() && <Upload value="Change photo" />}
-            </div>
-            <div className={styles.profile_info}>
-              <p className={styles.username}>{username}</p>
-              <p className={styles.post_count}>Posts: {user?.numberOfPosts}</p>
-              <p className={styles.reputation}>Reputation: {user?.reputation}</p>
-            </div>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div className={styles.profile}>
+            <p className={styles.profile_title}>Profile</p>
+            {isNotFound ? (
+              <div>Not Found user with username: {username}</div>
+            ) : (
+              <div className={styles.profile_content}>
+                <div className={styles.profile_media}>
+                  <Avatar imageUrl={resolveProfileImagePath(username)} customClass={styles.profile_avatar} />
+                  {isAuthorized && isPageOwner && <Upload value="Change photo" />}
+                </div>
+                <div className={styles.profile_info}>
+                  <p className={styles.username}>{username}</p>
+                  <p className={styles.post_count}>Posts: {user?.numberOfPosts}</p>
+                  <p className={styles.reputation}>Reputation: {user?.reputation}</p>
+                </div>
+              </div>
+            )}
           </div>
-        </div>
+        )}
       </Container>
     </React.Fragment>
   )
