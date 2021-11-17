1d998d99aa1402677ae0d2d1f24120e74bc734c0
KolomiietsAn <kkolomiiets@star.global>
2021-11-18 00:27:48 +0200

    feature/#79: Update spinner component

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,11 +1,9 @@
 import React, { Fragment } from 'react'
 
-import spinner from '../../assets/images/spinner.gif'
-
 import styles from './Spinner.module.scss'
 
 export const Spinner = () => (
   <Fragment>
-    <img src={spinner} alt="Loading..." className={styles.spinner} />
+    <div className={styles.loader}></div>
   </Fragment>
 )
