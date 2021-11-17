3b71accc18443a55fe7b9941534dc3f42479a657
Yana Gushlevskaya <jasynja@ukr.net>
2021-11-17 13:50:39 +0200

    add a new prop to Spinner component

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,11 +1,16 @@
+import cn from 'classnames'
 import React, { Fragment } from 'react'
 
 import spinner from '../../assets/images/spinner.gif'
 
 import styles from './Spinner.module.scss'
 
-export const Spinner = () => (
+interface Props {
+  className?: string
+}
+
+export const Spinner: React.FC<Props> = ({ className }) => (
   <Fragment>
-    <img src={spinner} alt="Loading..." className={styles.spinner} />
+    <img src={spinner} alt="Loading..." className={cn(styles.spinner, className)} />
   </Fragment>
 )
